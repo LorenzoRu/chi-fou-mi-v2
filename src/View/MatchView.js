1f235374e88de0a3abe163ca2e1a331f98d73cea
Lorenzo Ruocco <lorenzoruocco@MBP-de-Lorenzo.lan>
2023-04-19 15:32:42 +0200

    add move

--- src/View/MatchView.js
+++ src/View/MatchView.js
@@ -2,56 +2,112 @@ import React, { useContext, useEffect, useState } from "react";
 import { MatchContext } from "../contexts/MatchContext.js";
 import { useParams } from "react-router-dom";
 import { API_BASE_URL } from "../lib/constante.js";
-import { newFetch } from "../lib/fetch.js";
+import { AuthContext } from "../contexts/AuthContext.js";
 
 export default function MatchView() {
+  const { user } = useContext(AuthContext);
   const { getMatch, match } = useContext(MatchContext);
   const { id } = useParams();
   const [error, setError] = useState(null);
-  let i = 1;
+  const turn = match.turns && match.turns.length + 1;
+  console.log(turn);
 
   useEffect(() => {
     getMatch(id);
   }, [id]);
 
- async function handleRock() {
-  try {
-    const response = await fetch(`${API_BASE_URL}/matches/${id}/turns/${i}`, {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({
-        "move": "rock",
-      }),
-    });
-    if (response.status === 202) {
-      const data = await response.text();
-      console.log(data);
-      i++;
-      console.log(i);
-      getMatch(id);
-    } else {
-      const errorData = await response.json();
-      const error = errorData.match || errorData.user || errorData.turn;
-      throw new Error(error);
+  async function handleRock() {
+    try {
+      const response = await fetch(
+        `${API_BASE_URL}/matches/${id}/turns/${match.turns.length + 1}`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            move: "rock",
+          }),
+        }
+      );
+      if (response.status === 202) {
+        await response.text();
+        getMatch(id);
+      } else {
+        const errorData = await response.json();
+        const error = errorData.match || errorData.user || errorData.turn;
+        throw new Error(error);
+      }
+    } catch (error) {
+      setError(error.message);
     }
-  } catch (error) {
-    setError(error.message);
   }
- }
-
+  async function handlePaper() {
+    try {
+      const response = await fetch(
+        `${API_BASE_URL}/matches/${id}/turns/${turn}`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            move: "paper",
+          }),
+        }
+      );
+      if (response.status === 202) {
+        await response.text();
+        getMatch(id);
+      } else {
+        const errorData = await response.json();
+        const error = errorData.match || errorData.user || errorData.turn;
+        throw new Error(error);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+  async function handleScissors() {
+    try {
+      const response = await fetch(
+        `${API_BASE_URL}/matches/${id}/turns/${turn}`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            move: "scissors",
+          }),
+        }
+      );
+      if (response.status === 202) {
+        await response.text();
+        getMatch(id);
+      } else {
+        const errorData = await response.json();
+        const error = errorData.match || errorData.user || errorData.turn;
+        throw new Error(error);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  }
   return (
-    <div>
-      <h3>
-        Match: {match.user1 && match.user1.username} VS{" "}
-        {match.user2 && match.user2.username}{" "}
-      </h3>
-      <button onClick={handleRock}>Pierre</button>
-      <button>Papier</button>
-      <button>Ciseaux</button>
-      <p>Match ID: {match._id}</p>
-      <p>Winner: {match.winner && match.winner.username}</p>
-      <p>Turns: {match.turns && match.turns.length}</p>
-      {error && <p>{error}</p>}
-    </div>
+    <>
+      {user === false && (
+        <p>Vous devez être connecté pour accéder à cette page</p>
+      )}
+      {user !== false && (
+        <div>
+          <h3>
+            Match: {match.user1 && match.user1.username} VS{" "}
+            {match.user2 && match.user2.username}{" "}
+          </h3>
+          <button onClick={handleRock}>Pierre</button>
+          <button onClick={handlePaper}>Papier</button>
+          <button onClick={handleScissors}>Ciseaux</button>
+          <p>Match ID: {match._id}</p>
+          <p>Winner: {match.winner && match.winner.username}</p>
+          <p>Turns: {match.turns && match.turns.length}</p>
+          {error && <p>{error}</p>}
+        </div>
+      )}
+    </>
   );
 }
